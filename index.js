a681fc0019b3f64b2c72a1f2cd2a1934702e4213
Icyck <44445398+Icyck@users.noreply.github.com>
2018-10-26 10:42:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -22,26 +22,7 @@ robot.on("ready", () => {
     let cmd = messageArray[0]
     let args = messageArray.slice(1);
       
-    if(cmd === `${prefix}verify`){
     
-    let pMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]) ;
-    if(!pMember) return message.reply("Couldn't find rhat user, yo.");
-    
-    let gRole = message.guild.roles.find(`name`, "verify")
-    await(pMember.addRole(gRole.id));
-    if(!gRole) return message.reply("Couldn't find that role.");;
-
-    
-
-    try{
-    await    pMember.send(`Congrats to <@${pMember.id}> You verify account`);
-    }catch(e){
-    message.channel.send(`Congrats to <@${pMember.id}> you verify account!`);
-    }
-        
-        if(cmd === `${prefix}test){
-    message.reply("Test here");
-}
   });
   
 
